69fa1d503b3bc30b7585a1775fd36d182b082a28
ivana-stopka <ivana_stopka@hotmail.com>
2021-11-17 22:53:03 +1100

    first push of code for demographics and bar plot

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,50 @@
-// Use D3 fetch to read the JSON file
-// The data from the JSON file is arbitrarily named importedData as the argument
+// Used D3 fetch to read the JSON file and check structure of data by printing to the console.
 d3.json("data/samples.json").then((importedData) => {
   console.log(importedData);
-  var data = importedData;
 });
+
+// To build the dashboard, need data for the Demographics Table and data for the Plots which will change based on id.
+// So will create a function for the Demographics data and a function for the Plots data.
+
+// Demographics function
+function buildDemographics(id) {
+  d3.json("data/samples.json").then((data) => {
+    var metadata = data.metadata
+    var arr = metadata.filter(object =>
+      object.id == id);
+    var result = arr[0]
+    var panel = d3.select("#sample-metadata");
+    panel.html("");
+    Object.entries(result).forEach(([key,value]) => {
+      panel.append("h6").text(`${key}:${value}`);
+    });
+  });
+}
+
+// Plots function
+
+// Initialisation function
+function init() {
+  var menu = d3.select("#selDataset");
+
+  d3.json("data/samples.json").then((data) => {
+    var sampleNumber = data.names;
+    sampleNumber.forEach((id) => {
+      menu
+        .append("option")
+        .text(id)
+        .property("value",id);
+    });
+
+    const firstSampleNumber = sampleNumber[0];
+    buildDemographics(firstSampleNumber);
+  });
+}
+
+// Menu Change function
+function menuChange(newSampleNumber) {
+  buildDemographics(newSampleNumber);
+}
+
+// Initialise dashboard
+init();
